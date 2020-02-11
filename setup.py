9553ca299ed260731b9aef35bb9d04ee667fcd47
Ismail Sunni <imajimatika@gmail.com>
2020-02-11 17:14:39 +0100

    Commit pertama.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='itu',
+    version='0.0.9',
+    description='Indonesia version of import this.',
+    url='http://github.com/ismailsunni/itu',
+    author='Ismail Sunni',
+    author_email='imajimatika@gmail.com',
+    license='MIT',
+    packages=['itu'],
+    zip_safe=False)
